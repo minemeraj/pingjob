e3812a757ea1307fbba672595b37dc732735b6c0
mineme.shahriar@gmail.com <barshahriar2>
2016-11-23 21:10:45 +0000

    implemented....

--- src/main/java/io/shahriar/pingjob/Main.java
+++ src/main/java/io/shahriar/pingjob/Main.java
@@ -1,17 +1,30 @@
 package io.shahriar.pingjob;
 
 import com.mashape.unirest.http.Unirest;
-import com.mashape.unirest.http.exceptions.UnirestException;
 import com.mashape.unirest.request.GetRequest;
 
 public class Main {
     public static void main(String[] args) {
-        String url = "http://jenkins.shahriar.io/";
-GetRequest request = Unirest.get(url);
-
+        final String mainURL = "http://shahriar.io/";
+        final String jenkinsURL = "http://jenkins.shahriar.io/";
+        final String mavensURL = "http://maven.shahriar.io/";
+        GetRequest jenkinsrequest = Unirest.get(jenkinsURL);
+        GetRequest mavensrequest = Unirest.get(mavensURL);
+        GetRequest mainrequest = Unirest.get(mainURL);
     try {
-        System.out.println(request.asString().getStatus());
-    } catch (UnirestException e) {
+        while(jenkinsrequest.asString().getStatus() != 200) {
+            System.out.println("jenkins down... Waiting...");
+            Thread.sleep(3000);
+        }
+        while(mavensrequest.asString().getStatus() != 200) {
+            System.out.println("maven down... Waiting...");
+            Thread.sleep(3000);
+        }
+        while(mainrequest.asString().getStatus() != 200) {
+            System.out.println("maven down... Waiting...");
+            Thread.sleep(3000);
+        }
+    } catch (Exception e) {
         e.printStackTrace();
     }
     }
