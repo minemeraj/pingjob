6aa5e7f6ecda96afe479345c92da45242c6aa028
mineme.shahriar@gmail.com <barshahriar2>
2016-11-23 18:17:30 +0000

    gitignore...

--- src/main/java/io/shahriar/pingjob/Main.java
+++ src/main/java/io/shahriar/pingjob/Main.java
@@ -1,10 +1,18 @@
 package io.shahriar.pingjob;
 
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import com.mashape.unirest.request.GetRequest;
+
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {            
-        System.out.println("Hello");
-        System.out.println("Commit");
-        }
+        String url = "http://jenkins.shahriar.io/";
+GetRequest request = Unirest.get(url);
+
+    try {
+        System.out.println(request.asString().getStatus());
+    } catch (UnirestException e) {
+        e.printStackTrace();
+    }
     }
 }
